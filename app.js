f9395a16443fd234ff9399646b71a1cd35f71449
Santiago <santyalzateo05@gmail.com>
2023-12-02 13:16:10 -0500

    cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 const citaRoute = require("./routes/citas")
 const pedidoRoute = require("./routes/pedidos")
